62fb2e3f171c69729a8cb430eb643c626cac0dc0
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-08-26 08:19:08 +0100

    Add more tests, to verify that new objects are created.
    - new objects are created when expected
    - objects are not touched when expected

--- test/update.js
+++ test/update.js
@@ -4,7 +4,8 @@ import { update } from '../src';
 const state = {
   index : 1,
   nested: {a: 1, b: 2},
-  array : [2, 4, 6, 8]
+  array : [2, 4, 6, 8],
+  deep  : {d2: {d3: {d4: {d5: {value: 1}}}}}
 };
 
 describe('Update', () => {
@@ -15,7 +16,8 @@ describe('Update', () => {
       expect(state2).toEqual({
         index : 10,
         nested: {a: 1, b: 2},
-        array : [2, 4, 6, 8]
+        array : [2, 4, 6, 8],
+        deep  : {d2: {d3: {d4: {d5: {value: 1}}}}}
       });
     });
   });
@@ -27,7 +29,8 @@ describe('Update', () => {
       expect(state2).toEqual({
         index : 1,
         nested: {a: 1, b: 20},
-        array : [2, 4, 6, 8]
+        array : [2, 4, 6, 8],
+        deep  : {d2: {d3: {d4: {d5: {value: 1}}}}}
       });
     });
   });
@@ -39,8 +42,46 @@ describe('Update', () => {
       expect(state2).toEqual({
         index : 1,
         nested: {a: 1, b: 2},
-        array : [2, 4, 60, 8]
+        array : [2, 4, 60, 8],
+        deep  : {d2: {d3: {d4: {d5: {value: 1}}}}}
       });
+    });
+  });
+
+  describe('object', () => {
+    it('should mutate array', () => {
+      const state2 = update(state, ['array'], array => {
+        return [array.length, 1, 2, 3, 4, 5];
+      });
+
+      expect(state2).toEqual({
+        index : 1,
+        nested: {a: 1, b: 2},
+        array : [4, 1, 2, 3, 4, 5],
+        deep  : {d2: {d3: {d4: {d5: {value: 1}}}}}
+      });
+
+      expect(state2.array).toNotBe(state.array);
+      expect(state2.deep.d2.d3.d4.d5).toBe(state.deep.d2.d3.d4.d5);
+    });
+  });
+
+  describe('deep', () => {
+    it('should only change mutated and ancestors', () => {
+      const state2 = update(state, 'deep.d2.d3.d4.d5.value', value => value + 1);
+
+      expect(state2).toEqual({
+        index : 1,
+        nested: {a: 1, b: 2},
+        array : [2, 4, 6, 8],
+        deep  : {d2: {d3: {d4: {d5: {value: 2}}}}}
+      });
+
+      expect(state2.nested).toBe(state.nested);
+      expect(state2.array).toBe(state.array);
+
+      expect(state2.deep).toNotBe(state.deep);
+      expect(state2.deep.d2.d3.d4.d5).toNotBe(state.deep.d2.d3.d4.d5);
     });
   });
 });
